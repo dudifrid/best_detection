72f2f0aa4f3010410ef708c8ecb66a802a037fa6
dudifrid <dudifrid@gmail.com>
2022-10-03 20:12:13 +0300

    first tag

--- test.py
+++ test.py
@@ -59,7 +59,7 @@ def test(data,
         gs = max(int(model.stride.max()), 32)  # grid size (max stride)
         imgsz = check_img_size(imgsz, s=gs)  # check img_size
         
-        if trace:
+        if trace: 
             model = TracedModel(model, device, imgsz)
 
     # Half
